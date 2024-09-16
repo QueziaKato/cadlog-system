57afc662e00dc93e9c342fac918c45389c1aa4b7
QueziaKato <quezia.kato@alunoescolassociais.org.br>
2024-09-16 08:15:06 -0300

    Aqui foi feito um código que  inclui o arquivo user.php, que contém o modelo de usuário e suas funções para manipulação de dados. A classe AuthController é definida com um método login, que é responsável pelo processo de login, mas ainda não implementado.

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,5 +1,4 @@
 <?php // Requer arquivo 'user.php' que cont... por QUEZIA TAMIE KATO 07:40
-<?php
 // Requer arquivo 'user.php' que contem o model user com as funções para manupulação de dados de usuário.
 require_once 'models/user.php';
  
