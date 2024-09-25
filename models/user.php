ea2fa6dbc6825b7fc3fecde40cdefbeddc7b29aa
QueziaKato <quezia.kato@alunoescolassociais.org.br>
2024-09-25 10:51:06 -0300

    funçao cria banco de dados

--- models/user.php
+++ models/user.php
@@ -1,12 +1,30 @@
 <?php
-require_once 'models/database.php';
-
-class user 
-{
-  public static function  findByEmail($email) {
-    
-  }
-}
-
-
+require_once'models/database.php';
+class User
+    {
+        //função para localizar uruário pelo email
+        public static function findByEmail($email){
+            $conn = database::getConnection();
+            $stmt = $conn->prepare("SELECT*FROM usúarios WHERE email = :email");
+            $stmt->execute(['email' => $email]);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        // função para encontrar usuário pelo ID
+        public static function find ($id){
+            $conn = database::getConnection();
+            $stmt = $conn->prepare("SELECT*FROM usuarios WHERE id = :id");
+            $stmt->execute(['id => $id']);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        // função para criar um novo usuário no banco de dados
+        public static function create($data){
+            $conn = database::getConnection();
+            $stmt = $conn->prepare("");
+            $stmt = $conn->prepare("INSERT INTO usuarios
+             (nome, email, senha, perfil) VALUES (nome, email, senha, perfil)");
+             
+ 
+        }
+    }
+ 
 ?>
