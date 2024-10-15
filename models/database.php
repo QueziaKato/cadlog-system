0793b08712cfbe090f57bee577eb5cf3fdfa2fc5
QueziaKato <quezia.kato@alunoescolassociais.org.br>
2024-10-15 12:15:17 -0300

    corrigindo erro

--- models/database.php
+++ models/database.php
@@ -1,23 +1,22 @@
 <?php
 class Database{
-    //utiliza padrão singleton, cujo objetivo é garantir que apenas uma única instância  de classe seja criada durante a execução do programa, e que essa instância seja utilizada sempre que necesário,
+    // Utiliza padrão Singleton, cujo objtivo é garantir que apenas uma única instância de classe seja criada durante a execução do programa, e que essa instância seja utilizada sempre que necessário
     private static $instance = null;
  
-    // método público que retorna a conexão com BD
+    //Método público que retorna a conexão com BD
     public static function getConnection(){
-        // verifica se a instância de conexão ainda não criada
-if(!self::$instance){
-    $host     ='localhost';
-    $db       = 'sistema_usuario';
-    $user     = 'root';
-    $password ='';
-//a conexão usa o driver mysql (mysql) e as informações de host e DB
-self::$instance = new
-PDO("mysql:host=$host;dbname=$db", $user, $password);
-self::$instance->setAttribute(PDO::ATTR_ERMODE, PDO::ERMODE::EXCEPTION);
-} 
-return self::$instance;
-       
+        // Verifica se a instância de conexão ainda não foi criada
+        if(!self::$instance){
+            $host       = 'localhost';
+            $db         = 'sistema_usuarios';
+            $user       = 'root';
+            $password   = '';
+            // A conexão usa o driver Mysql (mysql:) e as informações de host e DB
+            self::$instance = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+            // Define o modo de erro para exceções, facilitando a depuração e tratamento do erro
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instance;
     }
 }
-?> 
+?>
