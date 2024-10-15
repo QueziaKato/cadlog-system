665c6469e85f2a64a61521fe997536d4b877e7de
QueziaKato <quezia.kato@alunoescolassociais.org.br>
2024-10-15 12:09:44 -0300

    modificando erros

--- routes.php
+++ routes.php
@@ -1,32 +1,33 @@
 <?php
- 
 // Inclui arquivos de controlador para lidar com diferentes ações
-require 'controllers/AuthController.php'; //inclui o controlador de autenticação
-require 'controllers/UserController.php'; //inclui o controlador se usuário
-require 'controllers/DashboardController.php'; //inclui o controlador de dashboard
+require 'controllers/AuthController.php'; // inclui o controlador de autenticação
+require 'controllers/UserController.php'; // inclui o controlador de usuário
+require 'controllers/DashboardController.php'; // inclui o controlador de dashboard
 
-// Cria instãncias dos controladores para utilizar seus métodos
-$authController = new AuthController(); //intancia o controlador de autenticação = para poder usar
-$useController = new $useController();
- 
-//Coleta a ação da URL, se não houver definida, usa 'login' por padrão
-$action = $_GET['action'] ?? 'login';  // Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
+// Cria instâncias dos controladores para utilizar seus métodos
+$authController = new AuthController(); // Instancia o controlador de autenticação
+$userController = new UserController();
+$dashboardController = new DashboardController();
 
-switch($action){
+
+// Coleta a ação da URL, se não houver definida, usa 'login' por padrão
+$action = $_GET['action'] ?? 'login'; // Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
+
+// Verifica a ação solicitada e chama o método apropriado do controlador
+switch ($action) {
     case 'login':
-        $authController->login(); // chama o método login do controlador de autentificação
+        $authController->login(); // chama o método login do controlador de autenticação
         break;
-        case 'register':
-            $useController->register();
-            break;
-            case 'dashboard':
-                $dashboardController->index();
-                break;
-                case 'logout':
-
-        default:
+    case 'register':
+        $userController->register();
+        break;
+    case 'dashboard':
+        $dashboardController->index();
+        break;
+    case 'logout':
+        $authController->logout();
+        break;
+    default:
         $authController->login();
         break;
 }
- 
-?>
