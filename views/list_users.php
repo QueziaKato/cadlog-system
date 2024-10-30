11a96050d9f54bf0f8fac09c469c8f5c081ee1a7
QueziaKato <quezia.kato@alunoescolassociais.org.br>
2024-10-30 10:10:08 -0300

    atualização do list user, codigo m

--- views/list_users.php
+++ views/list_users.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+ 
+    if(isset($_SESSION['perfil'])):
+?>
+ 
 <!DOCTYPE html>
 <html lang="pt-br">
  
@@ -8,7 +14,7 @@
     <link rel="stylesheet" type='text/css' media='screen' href="css/list.css"> <!-- Link para o arquivo CSS -->
 </head>
  
-<body class=""> <!-- Define a classe com base no perfil do usuário -->
+<body class="<?= $_SESSION['perfil']?>"> <!-- Define a classe com base no perfil do usuário -->
     <div class="container">
         <h2>Lista de Usuários</h2>
         <table class="styled-table">
@@ -22,32 +28,33 @@
                 </tr>
             </thead>
             <tbody>
+                <?php foreach ($users as $user) ?>
+                <tr>
+                    <td><?= $user['id'] ?></td>
+                    <td><?= $user['nome'] ?></td>
+                    <td><?= $user['email'] ?></td>
+                    <td><?= $user['perfil'] ?></td>
+                    <td><?php if($_SESSION['perfil']  == 'admin' || $_SESSION['perfil']  == 'gestor' ):?>
+                        <a href="">Editar</a>
+                        <?php endif; ?>
+                        <?php if($_SESSION['perfil']  == 'admin'): ?>
+                            <a href="">Excluir</a>
+                            <?php endif; ?>
  
- <?php foreach ($user as $user) : ?>
-     <tr>
-         <td><?= $user['id'] ?></td>
-         <td><?= $user['nome'] ?></td>
-         <td><?= $user['email'] ?></td>
-         <td><?= $user['perfil'] ?></td>
-         <td>
-             <!-- verifica se o perfil é admin ou gestor para poder adicionar ou não o botão editar -->
-             <?php if ($_SESSION['perfil'] == 'admin' || $_SESSION['perfil'] == 'gestor') : ?>
-                 <a href="index.php?edit&id">Editar</a>
-             <?php endif; ?>
-
-             <!-- insere o botão de exclusão apenas para o perfil admin -->
-             <?php if ($_SESSION['perfil'] == 'admin') : ?>
-                 <a href="">Excluir</a>
-
-             <?php endif; ?>
-         </td>
-     </tr>
- <?php endforeach; ?>
-</tbody>
-</table>
-
-<a href="index.php?action=dashboard" class="btn">Voltar ao Dashboard</a>
-</div>
+ 
+                    </td>
+                </tr>
+ 
+               
+            </tbody>
+        </table>
+ 
+        <a href="index.php?action=dashboard" class="btn">Voltar ao Dashboard</a>
+    </div>
 </body>
-
+ 
 </html>
+ 
+<?php else: ?>
+    <p>Erro: Você não tem permissão para visualizar essa página. </p>
+<?php endif; ?>
