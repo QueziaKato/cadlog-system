25e608093e4ac068a07fe204fc482932feb6a6a0
QueziaKato <quezia.kato@alunoescolassociais.org.br>
2024-10-30 09:43:53 -0300

    arrumando o codigo

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -9,45 +9,44 @@ class UserController
             $data = [
                 'nome' => $_POST['nome'],
                 'email' => $_POST['email'],
-                'senha' => password_hash($_POST['senha'], PASSWORD_DEFAULT),//criptografa a senha
+                'senha' => password_hash($_POST['senha'], PASSWORD_DEFAULT), //criptografa a senha
                 'perfil' => $_POST['perfil']
- 
+
             ];
- 
+
             User::create($data);
             header('Location: index.php');
- 
         } else {
             //Se nao for post carrega a pagina de registro
             include 'views/register.php';
         }
-       
     }
-    public function list(){
+    public function list()
+    {
         $users = User::all();
         include 'views/list_users.php';
-    }
-    public function edit($id){
+    } 
+
+    public function edit($id)
+    {
         session_start();
+        // permitir que o admin e gestor edite 
+        if ($_SESSION['perfil'] == 'admin' || $_SESSION['perfil'] == 'gestor') {
+            $user = User::find($id);
 
-      // Permitir que admin e gestor editem  
-      if ($_SESSION['perfil'] == 'admin' || $_SESSION['perfil'] == 'gestor'){
-        $user = User::find($id);
+            if ($_SERVER['RESQUET_METHOD'] == 'POST') {
+                $data = [
+                    'nome' => $_POST['nome'],
+                    'email' => $_POST['email'],
+                    'perfil' => $_POST['perfil']
 
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            $data = [
-                'nome' => $_POST['nome'],
-                'email' => $_POST['email'],
-                'perfil' => $_POST['perfil']
- 
-            ];
+                ];
 
-            User::update($id, $data);
-            header('Location')
 
+                User::update($id, $data);
+                header('Location: index.php?action=list');
+            }
         }
-      }
     }
 }
-?>
  
